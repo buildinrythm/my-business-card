fd6a670f51d3aea278269115991cdb83a5acbeb2
Joshua Agbor <123345261+Zaunsrevenge@users.noreply.github.com>
2025-07-22 20:31:05 +0100

    Added text for about and interest sections

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -10,6 +10,14 @@ export default function Info() {
    <button className="email-button">Email</button>
    <button className="linkedin-button">LinkedIn</button>
    </div>
+   <div className="About">  
+    <h3>About</h3>
+    <p>I am a frontend developer with a particular interest in making things simple and automating daily tasks. I try to keep up with security and best practices, and am always looking for new things to learn.</p>
+   </div>
+   <div className="Interests">
+    <h3>Interests</h3>
+    <p>Food expert. Music scholar. Reader. Internet fanatic. Bacon buff. Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.</p>
+   </div>
   </>
 )
 }

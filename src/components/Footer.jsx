a12818af4c89cfe103a272166783bea4294ee401
Joshua Agbor <123345261+Zaunsrevenge@users.noreply.github.com>
2025-07-22 20:44:57 +0100

    Styled footer icons

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,12 @@
 export default function Footer() {
   return (
     <>
-    <img src="src/assets/Twitter.svg" alt="twitter icon" />
+    <div className="footer-icon-container">
+    <img src="src/assets/Twitter.svg" alt="twitter icon" className="footer-icon"/>
+    <img src="src/assets/Facebook.svg" alt="facebook icon" className="footer-icon" />
+    <img src="src/assets/Instagram.svg" alt="instagram icon" className="footer-icon" />
+    <img src="src/assets/Github.svg" alt="github icon" className="footer-icon" />
+    </div>
   </>
 )
 }

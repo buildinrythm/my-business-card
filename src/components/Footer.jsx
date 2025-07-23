9752c1a177a16bcf571cca2ebb189f33758b5bdb
Joshua Agbor <123345261+Zaunsrevenge@users.noreply.github.com>
2025-07-23 01:50:15 +0100

    Fixed card container overflow. Changed background color to match figma design

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,10 @@
 export default function Footer() {
   return (
-    <>
     <div className="footer-icon-container">
     <img src="src/assets/Twitter.svg" alt="twitter icon" className="footer-icon"/>
     <img src="src/assets/Facebook.svg" alt="facebook icon" className="footer-icon" />
     <img src="src/assets/Instagram.svg" alt="instagram icon" className="footer-icon" />
     <img src="src/assets/Github.svg" alt="github icon" className="footer-icon" />
     </div>
-  </>
 )
 }

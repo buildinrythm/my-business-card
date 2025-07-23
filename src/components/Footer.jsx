642da821fd958c54ee22177f16ea998ae0eaecf6
Joshua Agbor <123345261+Zaunsrevenge@users.noreply.github.com>
2025-07-23 03:48:24 +0100

    Fixed footer icon imports

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,15 @@
+import Twittericon from '../assets/Twitter.svg';
+import Facebookicon from '../assets/Facebook.svg';
+import Instagramicon from '../assets/Instagram.svg';
+import Githubicon from '../assets/Github.svg';
+
 export default function Footer() {
   return (
     <div className="footer-icon-container">
-    <img src="src/assets/Twitter.svg" alt="twitter icon" className="footer-icon"/>
-    <img src="src/assets/Facebook.svg" alt="facebook icon" className="footer-icon" />
-    <img src="src/assets/Instagram.svg" alt="instagram icon" className="footer-icon" />
-    <img src="src/assets/Github.svg" alt="github icon" className="footer-icon" />
+    <img src={Twittericon} alt="twitter icon" className="footer-icon"/>
+    <img src={Facebookicon} alt="facebook icon" className="footer-icon" />
+    <img src={Instagramicon} alt="instagram icon" className="footer-icon" />
+    <img src={Githubicon} alt="github icon" className="footer-icon" />
     </div>
 )
 }
--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -12,7 +12,7 @@ export default function Info() {
    </div>
    <div className="card-buttons">
    <a href="mailto:joshuaagbor@gmail.com"><button className="email-button"><img src={Mailimg} alt="Mail"></img>Email</button></a>
-   <a href="www.linkedin.com/in/joshua-agbor-b06a37199" target="_blank"
+   <a href="https://www.linkedin.com/in/joshua-agbor-b06a37199" target="_blank"
   rel="noopener noreferrer"><button className="linkedin-button"><img src={LinkedinImg} alt="Linkedinlogo"></img>LinkedIn</button></a>
    </div>
    <div className="About">  
